c83602ab47d75981cee496ccda4f18aaaa6c6f06
Wentao Che <wentao.che@tum.de>
2025-07-05 11:23:13 +0200

    update env requirements

--- build_env.sh
+++ build_env.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # to write requirements.txt
-function write_requirements() {
+write_requirements() {
     pip freeze > requirements.txt
     conda list --explicit > environment.yml
-    echo "requirements.txt and evironment.yml have been created with the current environment's packages."
+    echo "requirements.txt and environment.yml have been created with the current environment's packages."
 }
 
 # write_requirements # uncomment to run
 
 # to install the requirements
-function install_requirements() {
+install_requirements() {
     if [ -f requirements.txt ]; then
         echo "Installing requirements from requirements.txt..."
         pip install -r requirements.txt
